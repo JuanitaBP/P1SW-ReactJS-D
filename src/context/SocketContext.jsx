85a053e3a39a7fe087531fff799a95619d260839
Juana Barriga Padilla <97569015+JuanitaBP@users.noreply.github.com>
2025-01-22 16:49:01 -0400

    Update SocketContext.jsx

--- src/context/SocketContext.jsx
+++ src/context/SocketContext.jsx
@@ -4,8 +4,8 @@ import { useSocket } from "../hooks/useSocket";
 export const SocketContext = createContext();
 
 export const SocketProvider = ({ children }) => {
-    //const url = `https://c4maker-server.herokuapp.com/`;
-    const url = 'http://localhost:8080'; 
+    const url = `https://diagramador-db-aeeb5fe8740f.herokuapp.com/`;
+    //const url = 'http://localhost:8080'; 
     const { socket, online } = useSocket(url);
 
     return (
