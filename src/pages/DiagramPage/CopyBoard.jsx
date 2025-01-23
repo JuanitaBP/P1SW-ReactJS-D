cc71f2150903de460ca057013f92a5fd30384861
Juana Barriga Padilla <97569015+JuanitaBP@users.noreply.github.com>
2025-01-22 22:41:40 -0400

    Update CopyBoard.jsx

--- src/pages/DiagramPage/CopyBoard.jsx
+++ src/pages/DiagramPage/CopyBoard.jsx
@@ -5,7 +5,8 @@ import { useParams } from 'react-router-dom';
 export const CopyBoard = () => {
     const params = useParams();
     //const link = `http://localhost:5173/diagram/${params.id}`;
-const link =  `https://diagramador-db-aeeb5fe8740f.herokuapp.com/diagram/${params.id}`;
+//const link =  `https://diagramador-db-aeeb5fe8740f.herokuapp.com/diagram/${params.id}`;
+   const link =  `https://vercel.com/juanitabps-projects/sw_diagramador/diagram/${params.id}`;
     // Estado para manejar la notificación de copiado
     const [copied, setCopied] = useState(false);
 
