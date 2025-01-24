fe8df838c4d2cafd23e1495a2c42c41cb2471434
Juana Barriga Padilla <97569015+JuanitaBP@users.noreply.github.com>
2025-01-23 20:24:46 -0400

    Update DBComponent.jsx

--- src/pages/DiagramPage/NodeType/DBComponent.jsx
+++ src/pages/DiagramPage/NodeType/DBComponent.jsx
@@ -1,15 +1,24 @@
 import React from 'react'
 
-export const DBComponent = ({ atributos, tipos, primaryKey }) => {
+export const DBComponent = ({ atributos, tipos, primaryKey, methods }) => {
     return (
         <div>
-            {atributos.map((atributo, index) => (
-                <div key={index}>
-                    {primaryKey[index] && "(pk) "}
-                    {atributo}: 
-                    {tipos[index]}
-                </div>
-            ))}
+            <div className="bg-black text-white">
+                
+                {atributos?.map((atributo, index) => (
+                    <div key={index}>
+                        {primaryKey[index] && "(pk) "}
+                        {atributo}: {tipos[index]}
+                    </div>
+                ))}
+            </div>
+            <div className="text-black methods-section " style={{backgroundColor: "#F8E6BCFF" }}>
+                {methods?.map((method, index) => (
+                    <div key={index}>
+                        {method.name}({method.params}): {method.returnType}
+                    </div>
+                ))}
+            </div>
         </div>
-    )
-}
+    );
+};
